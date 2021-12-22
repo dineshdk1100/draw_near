79b342e48ce298222cffd5adcc1c6043e640a576
Harikrishnan <harigopalan2000@gmail.com>
2021-12-22 19:39:55 +0530

    offline alert

--- lib/screens/language.dart
+++ lib/screens/language.dart
@@ -3,11 +3,9 @@ import 'package:draw_near/services/user-service.dart';
 import 'package:draw_near/util/color_theme.dart';
 import 'package:draw_near/util/constants.dart';
 import 'package:easy_localization/src/public_ext.dart';
-import 'package:flutter/material.dart';
-import 'package:theme_mode_handler/theme_mode_handler.dart';
+import 'package:flutter/materialandler.dart';
 
-class Language  extends StatefulWidget {
-
+class Language extends StatefulWidget {
   @override
   _language createState() => _language();
 }
@@ -15,24 +13,29 @@ class Language  extends StatefulWidget {
 class _language extends State<Language> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+        body: Center(
+            // SizedBox(height:60,),
 
-      body: Center(
-         // SizedBox(height:60,),
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Image.asset(
+            "assets/images/logo_transparent.png",
+            height: 150,
+          ),
+        ),
+        // Text(" CHOOSE YOUR APP LANGUAGE ", style: TextStyle(fontSize: 20,color: Colors.blue.shade300),),
+        Text(
+          " CHOOSE YOUR APP LANGUAGE ",
+          style: TextStyle(fontSize: 20, color: Color(pastelThemePrimaryValue)),
+        ),
 
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-
-        children : [
-      Padding(padding: const EdgeInsets.all(15.0),
-        child: Image.asset("assets/images/logo_transparent.png",height: 150,),
-      ),
-         // Text(" CHOOSE YOUR APP LANGUAGE ", style: TextStyle(fontSize: 20,color: Colors.blue.shade300),),
-          Text(" CHOOSE YOUR APP LANGUAGE ", style: TextStyle(fontSize: 20,color: Color(pastelThemePrimaryValue)),),
-
-          SizedBox(height: 20,),
+        SizedBox(
+          height: 20,
+        ),
         ListTile(
           isThreeLine: true,
           title: Text("change_lang".tr() + '  (மொழியை மாற்றவும்)'),
@@ -42,16 +45,15 @@ class _language extends State<Language> {
             value: context.locale.toString(),
             items: AVAILABLE_LANGUAGES.keys
                 .map((lang) => DropdownMenuItem(
-              enabled: lang == 'en_IN' ? true : false,
-              child: Text(
-                  AVAILABLE_LANGUAGES[lang] ?? 'unknown_lang'),
-              value: lang,
-            ))
+                      enabled: lang == 'en_IN' ? true : false,
+                      child: Text(AVAILABLE_LANGUAGES[lang] ?? 'unknown_lang'),
+                      value: lang,
+                    ))
                 .toList(),
           ),
         ),
-          Divider(),
-         /* ListTile(
+        Divider(),
+        /* ListTile(
             //isThreeLine: true,
               onTap: (){
 
@@ -94,42 +96,39 @@ class _language extends State<Language> {
               //subtitle: Text(UserService.instance.theme.toString().capitalize().tr()),
               trailing: Icon(Icons.arrow_forward_ios)
           ),*/
-          SizedBox(height: 20,),
-          Container(
-
-              margin: EdgeInsets.all(15),
-              width: 180,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                 // primary: Colors.blue.shade100, // background
-                  primary: Color(pastelThemePrimaryValue), // background
-                  onPrimary: Colors.white, // foreground
-                ),
-                onPressed: (){
-                  // if(hasValue()) {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (c) =>
-                          Onboarding(),
-                          ));
-                  // }
-
-                },
-
-
-                child: Text('OK',style: TextStyle(fontFamily: 'San Francisco',color: Colors.black,fontWeight: FontWeight.bold),),
-              )
-          )
-
-    ],
-      )
-    )
-
-
-    );
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+            margin: EdgeInsets.all(15),
+            width: 180,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                // primary: Colors.blue.shade100, // background
+                primary: Color(pastelThemePrimaryValue), // background
+                onPrimary: Colors.white, // foreground
+              ),
+              onPressed: () {
+                // if(hasValue()) {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (c) => Onboarding(),
+                ));
+                // }
+              },
+              child: Text(
+                'OK',
+                style: TextStyle(
+                    fontFamily: 'San Francisco',
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            ))
+      ],
+    )));
   }
 
-  onSelectLanguage(value){
-    if (value == null ) return;
+  onSelectLanguage(value) {
+    if (value == null) return;
     var localeTemp = value.toString().split('_');
     context.setLocale(Locale(localeTemp[0], localeTemp[1]));
     UserService.instance.locale = value;
