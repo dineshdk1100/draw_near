e95c0ae41d188f6a40ea954033832b1390bb5734
Harikrishnan <harigopalan2000@gmail.com>
2022-01-14 18:45:28 +0530

    fulldreen done

--- lib/screens/song-details.dart
+++ lib/screens/song-details.dart
@@ -1,10 +1,8 @@
 import 'package:draw_near/models/song.dart';
-import 'package:draw_near/screens/video-fullscreen.dart';
 import 'package:draw_near/services/song-service.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class SongDetails extends StatefulWidget {
@@ -28,71 +26,54 @@ class _SongDetailsState extends State<SongDetails> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('song'.tr()),
+    return YoutubePlayerBuilder(
+        player: YoutubePlayer(
+          controller: _controller,
+          controlsTimeOut: Duration(seconds: 3),
+          showVideoProgressIndicator: true,
         ),
-        body: YoutubePlayerBuilder(
-          player: YoutubePlayer(
-            controller: _controller,
-            controlsTimeOut: Duration(seconds: 10),
-            showVideoProgressIndicator: true,
-            bottomActions: [
-              const SizedBox(width: 14.0),
-              CurrentPosition(),
-              const SizedBox(width: 8.0),
-              ProgressBar(
-                isExpanded: true,
-                colors: ProgressBarColors(),
+        builder: (context, player) => Scaffold(
+              appBar: AppBar(
+                title: Text('song'.tr()),
               ),
-              RemainingDuration(),
-              const PlaybackSpeedButton(),
-              IconButton(icon: Icon(Icons.fullscreen, color: Colors.white,), onPressed: ()=> pushNewScreen(
-                context,
-                screen: VideoFullScreen(_controller),
-                withNavBar: false, // OPTIONAL VALUE. True by default.
-                pageTransitionAnimation: PageTransitionAnimation.cupertino,
-              ),),
-            ],
-          ),
-          builder: (context, player) => SizedBox.expand(
-            child: Container(
-              //decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-              //padding: EdgeInsets.all(16),
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'song'.tr() + ' ' + song.number,
-                      style: GoogleFonts.abrilFatface(
-                        textStyle: Theme.of(context).textTheme.headline4,
+              body: SizedBox.expand(
+                child: Container(
+                  //decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+                  //padding: EdgeInsets.all(16),
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'song'.tr() + ' ' + song.number,
+                          style: GoogleFonts.abrilFatface(
+                            textStyle: Theme.of(context).textTheme.headline4,
+                          ),
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      player,
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          song.body,
+                          style: GoogleFonts.lora(
+                              fontStyle: FontStyle.italic,
+                              fontSize: 20,
+                              height: 1.5),
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  player,
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      song.body,
-                      style: GoogleFonts.lora(
-                          fontStyle: FontStyle.italic,
-                          fontSize: 20,
-                          height: 1.5),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
-          ),
-        ));
+            ));
   }
 
   initializeVideoController() {
@@ -102,7 +83,6 @@ class _SongDetailsState extends State<SongDetails> {
       flags: YoutubePlayerFlags(
         autoPlay: false,
         mute: false,
-      controlsVisibleAtStart: true,
 
         //enableCaption: true,
       ),
--- lib/screens/video-fullscreen.dart
+++ lib/screens/video-fullscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoFullScreen extends StatefulWidget {
@@ -14,41 +13,44 @@ class VideoFullScreen extends StatefulWidget {
 class _VideoFullScreenState extends State<VideoFullScreen> {
   @override
   void initState() {
-      SystemChrome.setPreferredOrientations([
-        DeviceOrientation.landscapeLeft,
-        DeviceOrientation.landscapeRight,
-      ]);
+    // SystemChrome.setPreferredOrientations([
+    //   DeviceOrientation.landscapeLeft,
+    //   DeviceOrientation.landscapeRight,
+    // ]);
 
     super.initState();
   }
 
   @override
   void deactivate() {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     super.deactivate();
   }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Container(
-      child: AspectRatio(aspectRatio: 16/9,
-        child: YoutubePlayer(
-          controller: widget.controller,
-          controlsTimeOut: Duration(seconds: 1),
-          showVideoProgressIndicator: true,
-          bottomActions: [
-            const SizedBox(width: 14.0),
-            CurrentPosition(),
-            const SizedBox(width: 8.0),
-            ProgressBar(
-              isExpanded: true,
-              colors: ProgressBarColors(),
-            ),
-            RemainingDuration(),
-            const PlaybackSpeedButton(),
-            IconButton(icon: Icon(Icons.fullscreen_exit, color: Colors.white), onPressed: () {widget.controller.Navigator.pop(context);}),
-          ],
-        ),
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: widget.controller,
+        controlsTimeOut: Duration(seconds: 1),
+        showVideoProgressIndicator: true,
+        // bottomActions: [
+        //   const SizedBox(width: 14.0),
+        //   CurrentPosition(),
+        //   const SizedBox(width: 8.0),
+        //   ProgressBar(
+        //     isExpanded: true,
+        //     colors: ProgressBarColors(),
+        //   ),
+        //   RemainingDuration(),
+        //   const PlaybackSpeedButton(),
+        //   IconButton(
+        //       icon: Icon(Icons.fullscreen_exit, color: Colors.white),
+        //       onPressed: () {
+        //         Navigator.pop(context);
+        //       }),
+        // ],
       ),
-    ),);
+      builder: (context, player) => SafeArea(child: player),
+    );
   }
 }
