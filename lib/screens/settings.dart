1379c0a9670b4780f28468e25bd1544909b6c795
Jothiswaran A <jothiswaran2604@gmail.com>
2022-01-22 20:03:01 +0530

    Tamil translate - 100%

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -134,7 +134,7 @@ class _SettingsState extends State<Settings> with ScreenLoader {
             ListTile(
               //trailing: Icon(Icons.navigate_next),
               enabled: UserService.instance.isLoggedIn,
-              title: Text("Edit profile"),
+              title: Text("edit_profile".tr()),
               trailing: Icon(Icons.navigate_next),
               onTap: () {
                 Navigator.of(context)
@@ -193,7 +193,7 @@ class _SettingsState extends State<Settings> with ScreenLoader {
             Divider(),
             ListTile(
               minVerticalPadding: 0,
-              title: Text("daily_reminder".tr()),
+              title: Text('daily_reminder'.tr()),
               trailing: Switch.adaptive(
                   value: UserService.instance.isReminderOn,
                   onChanged: (newValue) => setState(() {
@@ -222,7 +222,7 @@ class _SettingsState extends State<Settings> with ScreenLoader {
             ),
             ListTile(
               minVerticalPadding: 0,
-              title: Text('Reminder not working ?'),
+              title: Text('reminder_not_working'.tr()),
               trailing: Icon(Icons.navigate_next),
               onTap: () {
                 //NotificationService.instance.showNotification();
